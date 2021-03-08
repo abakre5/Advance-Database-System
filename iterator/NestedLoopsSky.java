e959287b34584989d92ea5663d66f97c943f6cad
rrgore <rahul.ravi.gore@gmail.com>
2021-03-07 19:33:27 -0700

    Bug fix - created temp heap file for skyline operations

--- iterator/NestedLoopsSky.java
+++ iterator/NestedLoopsSky.java
@@ -26,6 +26,8 @@ public class NestedLoopsSky extends Iterator {
     private String relation_name;
     private short noOfColumns;
     private int noOfBufferPages;
+    private List<RID> skylineRIDList;
+    private Heapfile skyFile;
 
     public NestedLoopsSky(AttrType[] _in1,
                           int amt_of_mem,
@@ -52,6 +54,7 @@ public class NestedLoopsSky extends Iterator {
         pref_list = _pref_list;
         relation_name = _relationName;
         noOfBufferPages = n_pages;
+        skylineRIDList = new ArrayList<>();
 //        first_time = true;
         setSkyline();
     }
@@ -64,18 +67,36 @@ public class NestedLoopsSky extends Iterator {
      */
     private void setSkyline() throws Exception {
         if(!relation_name.isEmpty()) {
-            // We use outer as outer loop
-            // Creating new scan for inner loop
-            Heapfile f = null;
             try {
-                f = new Heapfile(relation_name);
+                skyFile = new Heapfile("skynls.in");
             } catch (Exception e) {
                 System.err.println(" Could not open heapfile");
                 e.printStackTrace();
             }
 
-            FileScan outerScan = getFileScan();
-            FileScan innerScan = getFileScan();
+            RID rid = new RID();
+            FileScan ogScan = getFileScan(relation_name);
+            Tuple t = null;
+            try {
+                t = ogScan.get_next();
+            } catch (Exception e) {
+                System.err.println(" Could not set tuple");
+                e.printStackTrace();
+            }
+            while(t != null) {
+                try {
+                    rid = skyFile.insertRecord(new Tuple(t).returnTupleByteArray());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                t = ogScan.get_next();
+            }
+            ogScan.close();
+
+            // Creating new scans for inner and outer loops
+            FileScan outerScan = getFileScan("skynls.in");
+            FileScan innerScan = getFileScan("skynls.in");
 
             TupleRIDPair outerTupleRid = outerScan.get_next1();
             while(outerTupleRid != null) {
@@ -86,7 +107,7 @@ public class NestedLoopsSky extends Iterator {
                     RID innerRid = innerTupleRid.getRID();
                     if( TupleUtils.Dominates( outerTuple, in1, innerTuple, in1, (short)in1.length, new short[0], pref_list, pref_list.length ) ) {
                         try {
-                            f.deleteRecord(innerRid);
+                            skyFile.deleteRecord(innerRid);
                         } catch (Exception e) {
                             System.err.println("*** Error deleting record \n");
                             e.printStackTrace();
@@ -96,26 +117,26 @@ public class NestedLoopsSky extends Iterator {
                     innerTupleRid = innerScan.get_next1();
                 }
                 innerScan.close();
-                innerScan = getFileScan();
+                innerScan = getFileScan("skynls.in");
                 outerTupleRid = outerScan.get_next1();
             }
             outerScan.close();
 
-            // For now, scan and store in skyline ArrayList
-            outer = getFileScan();
+            // Scan and store in skyline heap file
+            outer = getFileScan("skynls.in");
         } else {
             System.out.println("ERROR: Relation name not specified");
         }
     }
 
-    private FileScan getFileScan() throws IOException, FileScanException, TupleUtilsException, InvalidRelation {
+    private FileScan getFileScan(String relation) throws IOException, FileScanException, TupleUtilsException, InvalidRelation {
         FileScan scan;
 
         FldSpec[] Pprojection = new FldSpec[noOfColumns];
         for (int i = 1; i <= noOfColumns; i++) {
             Pprojection[i - 1] = new FldSpec(new RelSpec(RelSpec.outer), i);
         }
-        scan = new FileScan(relation_name, in1, new short[0],
+        scan = new FileScan(relation, in1, new short[0],
                 noOfColumns, noOfColumns, Pprojection, null);
         return scan;
     }
@@ -124,10 +145,16 @@ public class NestedLoopsSky extends Iterator {
     public Tuple get_next() throws IOException, JoinsException, IndexException, InvalidTupleSizeException, InvalidTypeException, PageNotReadException, TupleUtilsException, PredEvalException, SortException, LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception {
         TupleRIDPair currTupleRid = outer.get_next1();
         if(currTupleRid == null) {
+            // Deleting records in temp heap file
+            for(RID skyRid : skylineRIDList) {
+                skyFile.deleteRecord(skyRid);
+            }
             outer.close();
             return null;
         }
         Tuple currTuple = currTupleRid.getTuple();
+        RID currRid = currTupleRid.getRID();
+        skylineRIDList.add(currRid);
         return currTuple;
     }
 
