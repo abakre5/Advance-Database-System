8cd4175b278cc5217279a65c7f4bb17ccb1eea7d
Vasudev <4783721+vasudevm@users.noreply.github.com>
2021-04-19 14:11:24 -0700

    created IntKey and StrKey classes for hash index

---
+++ hash/IntKey.java
@@ -0,0 +1,34 @@
+package hash;
+
+public class IntKey extends KeyClass {
+    private int key;
+
+    public String toString() {
+        return "" + key;
+    }
+
+    /**
+     * Class constructor
+     *
+     * @param value the value of the integer key to be set
+     */
+    public IntKey(int value) {
+        key = value;
+    }
+
+    /**
+     * get a copy of the float key
+     *
+     * @return the reference of the copy
+     */
+    public int getKey() {
+        return key;
+    }
+
+    /**
+     * set the float key value
+     */
+    public void setKey(int value) {
+        key = value;
+    }
+}
---
+++ hash/StrKey.java
@@ -0,0 +1,34 @@
+package hash;
+
+public class StrKey extends KeyClass {
+    private String key;
+
+    public String toString() {
+        return key;
+    }
+
+    /**
+     * Class constructor
+     *
+     * @param value the value of the integer key to be set
+     */
+    public StrKey(String value) {
+        key = value;
+    }
+
+    /**
+     * get a copy of the float key
+     *
+     * @return the reference of the copy
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * set the float key value
+     */
+    public void setKey(String value) {
+        key = value;
+    }
+}
